229ebc6e6cfd3114c2ae2487a7cc3c0a5a69dc52
Avitesh <92258530+DevAvii@users.noreply.github.com>
2024-09-20 16:09:35 +0530

    Create avii.js

---
+++ src/ScreenRoom1/SubScreen1/avii.js
@@ -0,0 +1,18 @@
+// Import the 'readline' module for reading input from the command line
+const readline = require('readline');
+
+// Create an interface for reading input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Ask for the user's name
+rl.question("Please enter your name: ", function(name) {
+  // Log the response
+  console.log("hi!");
+  console.log("My name is Anthony Ghonzalvez.");
+  
+  // Close the readline interface
+  rl.close();
+});
